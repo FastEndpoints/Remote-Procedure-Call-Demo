7938726fb96f6beaa7097d218639b3fdab116321
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2025-12-15 11:21:21 +0530

    switch back to nugets

--- Test/TestFixture.cs
+++ Test/TestFixture.cs
@@ -9,27 +9,31 @@ public class TestFixture : IDisposable
 {
     public HttpClient StoreFrontClient { get; set; }
 
-    readonly WebApplicationFactory<Warehouse.Program> _warehouse = new();
-    readonly WebApplicationFactory<StoreFront.Program> _storefront = new();
+    readonly WebApplicationFactory<Warehouse.Program> _warehouse = new();   //this is the grpc server that hosts command handlers
+    readonly WebApplicationFactory<StoreFront.Program> _storefront = new(); //this is the client app that initiates command executions
 
     public TestFixture()
     {
+        //get a reference to the TestServer of the grpc handler server/app
         var warehouse = _warehouse.WithWebHostBuilder(
             c =>
             {
                 c.ConfigureTestServices(
                     s =>
                     {
+                        //fake command handlers can be registered for commands
                         s.RegisterTestCommandHandler<SayHelloCommand, TestCommandHandler>();
                     });
             }).Server;
 
+        //create and store a httpclient for calling endpoints on the client app
         StoreFrontClient = _storefront.WithWebHostBuilder(
             c =>
             {
                 c.ConfigureTestServices(
                     s =>
                     {
+                        //connect the test remote/grpc server to the client app
                         s.RegisterTestRemote(warehouse);
                     });
             }).CreateClient();
